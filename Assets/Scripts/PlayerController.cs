68844e317eea5410bfd45e3d846c4d7193152024
Philip <philnord.94@gmail.com>
2016-02-25 12:40:41 +0100

    First init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+
+{
+    Rigidbody rigidBody;
+    public float speed;
+    public float m_gravity;
+    private Vector3 lowGravity = Vector3.zero;
+
+    void Start() {
+        rigidBody = this.GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        lowGravity += Physics.gravity * m_gravity * Time.fixedTime;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidBody.MovePosition(rigidBody.position + movement * Time.time * speed);
+        
+    }
+}
